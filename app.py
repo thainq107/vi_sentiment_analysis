8644b428a7bc9a6c627969b1878c770fd5d56928
ThaiNQ <110602193+thainq107@users.noreply.github.com>
2024-12-04 22:56:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import re
+import string
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,6 +11,47 @@ from torchtext.vocab import build_vocab_from_iterator, Vocab
 
 tokenizer = get_tokenizer("basic_english")
 idx2label = {0: 'negative', 1:'positive'}
+
+def preprocess_text(text):
+    # remove URLs https://www.
+    url_pattern = re.compile(r'https?://\s+\wwww\.\s+')
+    text = url_pattern.sub(r" ", text)
+
+    # remove HTML Tags: <>
+    html_pattern = re.compile(r'<[^<>]+>')
+    text = html_pattern.sub(" ", text)
+
+    # remove puncs and digits
+    replace_chars = list(string.punctuation + string.digits)
+    for char in replace_chars:
+        text = text.replace(char, " ")
+
+    # remove emoji
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U0001F1F2-\U0001F1F4"  # Macau flag
+        u"\U0001F1E6-\U0001F1FF"  # flags
+        u"\U0001F600-\U0001F64F"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U0001F1F2"
+        u"\U0001F1F4"
+        u"\U0001F620"
+        u"\u200d"
+        u"\u2640-\u2642"
+        "]+", flags=re.UNICODE)
+    text = emoji_pattern.sub(r" ", text)
+
+    # normalize whitespace
+    text = " ".join(text.split())
+
+    # lowercasing
+    text = text.lower()
+    return text
 
 class TextCNN(nn.Module):
     def __init__(
@@ -56,6 +99,7 @@ model = load_model('text_cnn_model.pt')
 vocabulary = torch.load('vocabulary.pth')
 
 def inference(sentence, vocabulary, model):
+    sentence = preprocess_text(sentence)
     encoded_sentence = vocabulary(tokenizer(sentence))
     encoded_sentence = torch.tensor(encoded_sentence)
     encoded_sentence = torch.unsqueeze(encoded_sentence, 1)
